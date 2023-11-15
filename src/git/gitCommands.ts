936441c1b6cc6091160d191028293434e8422055
Elias Afara <afaraelias@gmail.com>
2023-11-15 13:16:52 +0100

    Refactor gitCommands.ts and add tests for isGitInstalled() function

--- src/git/gitCommands.ts
+++ src/git/gitCommands.ts
@@ -1,9 +1,16 @@
 import { execSync } from "child_process";
 
-export function checkGitInstalled(): void {
+export function isGitInstalled(): boolean {
   try {
     execSync("git --version", { stdio: "ignore" });
+    return true;
   } catch (e) {
+    return false;
+  }
+}
+
+export function checkGitInstalled(): void {
+  if (!isGitInstalled()) {
     console.error("Git is not installed. Please install Git.");
     process.exit(1);
   }
