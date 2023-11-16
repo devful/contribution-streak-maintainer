7be950bdaac395bf6d385f2a28faef1621932b20
Elias Afara <afaraelias@gmail.com>
2023-11-16 14:26:33 +0100

    Refactor GitHub API and add Git utilities

--- src/github/githubApi.ts
+++ src/github/githubApi.ts
@@ -1,9 +1,5 @@
 import { Octokit } from "octokit";
-import { execSync } from "child_process";
-import { getFormattedDate } from "../utils/dateUtils";
-import { createFileIfNotExists } from "../utils/fileUtils";
 import type { ContributionResponse } from "./githubTypes";
-import { checkGitInstalled } from "../git/gitCommands";
 
 export async function getContributions(
   username: string,
@@ -20,28 +16,6 @@ export async function getContributions(
   const todayContributions = findTodayContributions(response);
 
   return todayContributions?.contributionCount || 0;
-}
-
-export async function generateAndPushCommits() {
-  checkGitInstalled();
-
-  const filename = "AUTOMATED_CONTRIBUTIONS.md";
-  createFileIfNotExists(filename);
-
-  const randomCommitCount = Math.floor(Math.random() * 3) + 1;
-
-  for (let i = 1; i <= randomCommitCount; i++) {
-    const commitMessage = getCommitMessage(i);
-
-    // Append the commit message to AUTOMATED_CONTRIBUTIONS.md
-    execSync(`echo "${commitMessage}" >> ${filename}`);
-
-    // Stage and commit changes
-    execSync(`git add ${filename}`);
-    execSync(`git commit -m "${commitMessage}"`);
-  }
-
-  execSync("git push");
 }
 
 function validateUsername(username: string): void {
@@ -80,10 +54,4 @@ function findTodayContributions(response: ContributionResponse):
   return response.user.contributionsCollection.contributionCalendar.weeks
     .flatMap((week) => week.contributionDays)
     .find((day) => day.date.split("T")[0] === today);
-}
-
-function getCommitMessage(index: number): string {
-  const currentDate = new Date();
-  const formattedDate = getFormattedDate(currentDate);
-  return `Commit: ${formattedDate} #${index}`;
 }
